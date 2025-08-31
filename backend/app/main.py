067849c939e04f6e0ae9b456d29f181d30fdc8bd
fatezi404 <w1v20018@gmail.com>
2025-09-01 02:23:57 +0700

    feat: new routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
-from app.api.v1.routers import user
-
-app = FastAPI(docs_url="/api/v1/docs")
+from app.api.v1.routers import user, permission, response, option, survey, role, login, question
 
 
-@app.get("/", include_in_schema=False)
+app = FastAPI(docs_url='/api/v1/docs')
+
+
+@app.get('/', include_in_schema=False)
 async def root():
-    return JSONResponse(status_code=status.HTTP_200_OK, content="Root page")
+    return JSONResponse(status_code=status.HTTP_200_OK, content='Root page')
 
 
-@app.get("/api/v1/healthcheck", include_in_schema=False)
+@app.get('/api/v1/healthcheck', include_in_schema=False)
 async def healthcheck():
-    return JSONResponse(status_code=status.HTTP_200_OK, content="OK")
+    return JSONResponse(status_code=status.HTTP_200_OK, content='OK')
 
-app.include_router(router=user.router, prefix='/api/v1/user')
+
+app.include_router(router=user.router, prefix='/api/v1/user', tags=['User'])
+app.include_router(router=survey.router, prefix='/api/v1/survey', tags=['Survey'])
+app.include_router(router=permission.router, prefix='/api/v1/permission', tags=['Permission'])
+app.include_router(router=response.router, prefix='/api/v1/response', tags=['Response'])
+app.include_router(router=option.router, prefix='/api/v1/option', tags=['Option'])
+app.include_router(router=role.router, prefix='/api/v1/role', tags=['Role'])
+app.include_router(router=login.router, prefix='/api/v1/login', tags=['Login'])
+app.include_router(router=question.router, prefix='/api/v1/question', tags=['Question'])
