1587967cb25e3a8b272be42a21fd0129e58d31ba
fatezi404 <w1v20018@gmail.com>
2025-09-16 03:11:41 +0700

    fix

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.answer_model import Answer
+from app.models.option_model import Option
+from app.models.permission_model import Permission
+from app.models.question_model import Question
+from app.models.response_model import Response
+from app.models.role_model import Role
+from app.models.survey_model import Survey
+from app.models.user_model import User
+
+__all__ = ['Answer', 'Option', 'Permission', 'Question', 'Response', 'Role', 'Survey', 'User']
