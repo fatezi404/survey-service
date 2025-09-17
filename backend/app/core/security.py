8f2c07ae7b7960a98c5f942d198da9bb664b5c87
fatezi404 <w1v20018@gmail.com>
2025-09-17 08:59:13 +0700

    format fixes

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,10 +6,12 @@ from uuid import uuid4
 
 from app.core.config import settings, ALGORITHM, TokenType
 
+
 def get_hashed_password(password: str | bytes) -> str:
     if isinstance(password, str):
         password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt()).decode()
+
 
 def verify_password(plain_password: str | bytes, hashed_password: str | bytes) -> bool:
     if isinstance(plain_password, str):
@@ -17,6 +19,7 @@ def verify_password(plain_password: str | bytes, hashed_password: str | bytes) -
     if isinstance(hashed_password, str):
         hashed_password = hashed_password.encode()
     return bcrypt.checkpw(plain_password, hashed_password)
+
 
 def create_access_token(subject: int, expires_delta: timedelta | None = None) -> str:
     if expires_delta:
@@ -28,9 +31,10 @@ def create_access_token(subject: int, expires_delta: timedelta | None = None) ->
         'jti': str(uuid4()),
         'exp': expire,
         'iat': datetime.now(),
-        'type': TokenType.ACCESS
+        'type': TokenType.ACCESS,
     }
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+
 
 def create_refresh_token(subject: int, expires_delta: timedelta | None = None) -> str:
     if expires_delta:
@@ -42,19 +46,10 @@ def create_refresh_token(subject: int, expires_delta: timedelta | None = None) -
         'jti': str(uuid4()),
         'exp': expire,
         'iat': datetime.now(),
-        'type': TokenType.REFRESH
+        'type': TokenType.REFRESH,
     }
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
 
+
 def decode_token(token: str) -> dict:
     return jwt.decode(token, settings.SECRET_KEY, ALGORITHM)
-
-#async def blacklist_token(token: str):
-#    try:
-#        payload = decode_token(token)
-#        jti = payload.get('jti')
-#        exp = payload.get('exp')
-#        ttl = exp - int(datetime.now().timestamp())
-#        if jti:
-#            await
-#
