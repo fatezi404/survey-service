d0a646c3cdcf2a0b2ed6a78815eafb702ef89ff4
fatezi404 <w1v20018@gmail.com>
2025-09-17 09:01:49 +0700

    add routers to app

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,11 +17,11 @@ async def healthcheck():
     return JSONResponse(status_code=status.HTTP_200_OK, content='OK')
 
 
-app.include_router(router=user.router, prefix='/api/v1/user', tags=['User'])
-app.include_router(router=survey.router, prefix='/api/v1/survey', tags=['Survey'])
-app.include_router(router=permission.router, prefix='/api/v1/permission', tags=['Permission'])
-app.include_router(router=response.router, prefix='/api/v1/response', tags=['Response'])
-app.include_router(router=option.router, prefix='/api/v1/option', tags=['Option'])
-app.include_router(router=role.router, prefix='/api/v1/role', tags=['Role'])
+app.include_router(router=user.router, prefix='/api/v1/users', tags=['User'])
+app.include_router(router=role.router, prefix='/api/v1/roles', tags=['Role'])
+app.include_router(router=permission.router, prefix='/api/v1/permissions', tags=['Permission'])
+app.include_router(router=survey.router, prefix='/api/v1/surveys', tags=['Survey'])
+app.include_router(router=question.router, prefix='/api/v1/questions', tags=['Question'])
+app.include_router(router=option.router, prefix='/api/v1/options', tags=['Option'])
+app.include_router(router=response.router, prefix='/api/v1/responses', tags=['Response'])
 app.include_router(router=login.router, prefix='/api/v1/login', tags=['Login'])
-app.include_router(router=question.router, prefix='/api/v1/question', tags=['Question'])
